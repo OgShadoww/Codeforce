44c6673189d246af2f47d3781a733175df08e8e5
Orest <galenzao@gmail.com>
2025-04-14 23:21:13 +0300

    Trying to solve

---
+++ 800/2091A_OlympiadDate.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  unordered_map<int, int> map = {0: 3, 1: 1, 2: 2, 3: 1, 5: 1};
+  int t;
+  cin >> t;
+  while(t-- > 0) {
+    int n;
+    cin >> a;
+
+  }
+
+  return 0;
+}
