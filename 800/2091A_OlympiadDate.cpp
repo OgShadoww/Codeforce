8c247349028440de967b89f024432c9475aea471
Orest <galenzao@gmail.com>
2025-04-18 08:25:11 +0300

    First try

--- 800/2091A_OlympiadDate.cpp
+++ 800/2091A_OlympiadDate.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
+#include <unordered_map>
 using namespace std;
 
 // Idea: the full amount of variations is A(20; 8) that less than 10^6. So greed search can help
 // Second idea to use hash map
 
 int main() {
+  unordered_map<int, int> map;
 
   return 0;
 }
