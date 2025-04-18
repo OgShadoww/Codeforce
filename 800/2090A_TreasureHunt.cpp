c4851049e587b841c61f5f7a161acb3eb5c13021
Orest <galenzao@gmail.com>
2025-04-18 10:54:34 +0300

    Final version

--- 800/2090A_TreasureHunt.cpp
+++ 800/2090A_TreasureHunt.cpp
@@ -5,36 +5,23 @@ int main() {
   int t;
   cin >> t;
 
-  while(t-- > 0) {
+  while (t--) {
     long long x, y, a;
-    int k = 0;
-    long long depth = 0;
     cin >> x >> y >> a;
-    a += 1;
-    
-    
-    while(true) {
-      if(k % 2 == 0) {
-        depth += x;
-      }
-      if(k % 2 != 0) {
-        depth += y;
-      }
 
-      if(depth > a) {
-        if(k % 2 == 0) {
-          cout << "NO" << endl;
-          break;
-        }
-        else {
-          cout << "YES" << endl;
-          break;
-        }
-      }
+    x *= 2;
+    y *= 2;
+    a = a * 2 + 1;
 
-      k++;
+    long long depth = (a / (x + y)) * (x+y);
+
+    if((depth + x) >= a) {
+      cout << "NO" << endl;
+    }
+    else {
+      cout << "YES" << endl;
     }
   }
-    
+
   return 0;
 }
